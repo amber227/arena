8d62691e3c440a7bbd7c864adc8610315f9effef
amber227 <jyulin2000@protonmail.com>
2026-01-12 16:12:12 -0500

    accept conda TOS

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+~/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 ~/miniconda3/bin/conda init bash
 source ~/.bashrc
 
