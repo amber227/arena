5581e4f76846c77583aa3dc4c2c43413296bbf1c
amber227 <jyulin2000@protonmail.com>
2026-01-12 16:45:34 -0500

    add install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ source ~/.bashrc
 # Create & activate the Conda environment
 ~/miniconda3/bin/conda create -n arena-env python=3.11 -y
 ~/miniconda3/bin/conda activate arena-env
-~/miniconda3/envs/arena-env/bin/pip install -r ~/ARENA_3.0/requirements.txt 
+~/miniconda3/envs/arena-env/bin/pip install -r requirements.txt 
 ~/miniconda3/bin/conda install -n arena-env ipykernel --update-deps --force-reinstall -y
